238a0b4e4db349b418b340a15ca1eb3381f86cc7
Riquelme Maia Rodrigues <riquelmemaia2@gmail.com>
2023-07-30 23:53:24 -0300

    change base url api

--- src/configs/services/api.ts
+++ src/configs/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	// baseURL: 'https://todo-list-backend-demo.onrender.com/',
-	baseURL: 'http://localhost:5001/',
+	baseURL: 'https://todo-list-backend-demo.onrender.com/',
+	// baseURL: 'http://localhost:5001/',
 });
